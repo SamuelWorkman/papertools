32f074aed96f41157080f48be02880757002b87b
SamuelWorkman <samuel.workman@ou.edu>
2020-03-27 14:59:23 -0500

    Add Nord light template.

---
+++ R/nord_light_paper.R
@@ -0,0 +1,11 @@
+nord_light_paper <- function(toc = TRUE) {
+
+  css <- system.file("rmarkdown/templates/nord_light_paper/resources/nord_light.css", package = "papertools")
+
+  # call the base html_document function
+  pagedown::html_paged(fig_width = 6.5,
+                           fig_height = 4,
+                           highlight = NULL,
+                           theme = NULL,
+                           css = css)
+}
