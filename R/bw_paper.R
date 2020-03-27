459b417d98d232739428dcd538a84a65fbce94b0
SamuelWorkman <samuel.workman@ou.edu>
2020-03-27 13:19:51 -0500

    Renaming css for function call.

--- R/bw_paper.R
+++ R/bw_paper.R
@@ -3,7 +3,7 @@ bw_paper <- function(toc = TRUE) {
   css <- system.file("rmarkdown/templates/bw_paper/resources/bw.css", package = "papertools")
 
   # call the base html_document function
-  pagedown::paged_html(fig_width = 6.5,
+  pagedown::html_paged(fig_width = 6.5,
                            fig_height = 4,
                            highlight = NULL,
                            theme = NULL,
