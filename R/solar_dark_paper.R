4df75e11c5a75c62463e35111f7f693cf0ccabaa
SamuelWorkman <samuel.workman@ou.edu>
2020-03-27 13:42:32 -0500

    Add solarized light and dark templates.

---
+++ R/solar_dark_paper.R
@@ -0,0 +1,11 @@
+solar_dark_paper <- function(toc = TRUE) {
+
+  css <- system.file("rmarkdown/templates/bw_paper/resources/solar_dark_paper.css", package = "papertools")
+
+  # call the base html_document function
+  pagedown::html_paged(fig_width = 6.5,
+                           fig_height = 4,
+                           highlight = NULL,
+                           theme = NULL,
+                           css = css)
+}
